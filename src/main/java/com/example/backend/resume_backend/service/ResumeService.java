6341af380aaef8f5b3f14b4299973b34a9064806
Anshul <anshulbhaskar50@gmail.com>
2025-08-16 16:48:54 +0530

    Resume response using jsn

--- src/main/java/com/example/backend/resume_backend/service/ResumeService.java
+++ src/main/java/com/example/backend/resume_backend/service/ResumeService.java
@@ -1,8 +1,9 @@
 package com.example.backend.resume_backend.service;
 
+import java.io.IOException;
+
 public interface ResumeService {
 
-    String generateResumeResponse(String userResumedescription);
-
+    String generateResumeResponse(String userResumeDescription) throws IOException;
 
 }
