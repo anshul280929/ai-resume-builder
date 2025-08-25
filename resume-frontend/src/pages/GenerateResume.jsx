9c2c497fbdeef7887d81d8ff4ab0bd0f4e9f5c0d
Anshul <anshulbhaskar50@gmail.com>
2025-08-25 23:08:39 +0530

    ShowForm

--- resume-frontend/src/pages/GenerateResume.jsx
+++ resume-frontend/src/pages/GenerateResume.jsx
@@ -1,24 +1,21 @@
-import React, { useState } from 'react';
-import { FaBrain, FaTrash, FaPaperPlane, FaBook } from 'react-icons/fa';
+import React, { useEffect, useState } from 'react';
+import { FaBrain, FaTrash, FaPaperPlane, FaBook, FaPlusCircle } from 'react-icons/fa';
+import {useFieldArray, useForm} from 'react-hook-form';
 import { generateResume } from '../api/resumeService';
 import toast from 'react-hot-toast';
 
-const DescriptionInputPage = () => {
+const GenerateResume = () => {
+
+  const {register, control, handleSubmit} = useForm({
+    defaultValues: initialData,
+  });
+
+  const onSubmit = (data) => {
+    console.log('Form Data:', data);
+  };
+
   const [description, setDescription] = useState('');
   const [loading,setLoading]=useState(false);
-
-  function handleChange(event) {
-
-    const personalInformation = {
-      ...data.personalInformation,
-      [event.target.name]: event.target.value
-     };
-
-    setData({
-      ...data,
-      personalInformation: personalInformation,
-  });
-}
 
   //Personal infromation
   const [data,setData]=useState({
@@ -33,6 +30,28 @@ const DescriptionInputPage = () => {
     },
   });
 
+  
+
+  function handleChange(event) {
+    //Changing personal information
+    const personalInformation = {
+      ...data.personalInformation,
+      [event.target.name]: event.target.value
+     };
+
+    setData({
+      ...data,
+      personalInformation: personalInformation,
+    });
+  }
+
+  useEffect(() => {
+    console.log('Personal Information Data:', data);
+  }, [data]);
+
+
+
+
   const handleGenerate = async() => {
     alert('Generating your resume...');
 
@@ -40,6 +59,7 @@ const DescriptionInputPage = () => {
         setLoading(true);
         const responseData=await generateResume(description);
         console.log('Resume Data:', responseData);
+        setData(responseData);
         toast.success('Resume generated successfully!');
         
     } catch (error) {
@@ -55,90 +75,135 @@ const DescriptionInputPage = () => {
     setDescription('');
   };
 
+  const renderInput = (name, label, type = "text") => (
+    <div className="form-control w-full  mb-4">
+      <label className="label">
+        <span className="label-text text-base-content">{label}</span>
+      </label>
+      <input
+        type={type}
+        {...register(name)}
+        className="input input-bordered rounded-xl w-full bg-base-100 text-base-content"
+      />
+    </div>
+  );
+
+  const renderArrayInput = (name,label) => {
+    return (
+      <div className="form-control w-full max-w-lg mb-4">
+        <h3 className="text-xl font-semibold">{label}</h3>
+        {fields.fields.map((field, index) => (
+          <div key={field.id} className="p-4 rounded-lg mb-4 bg-base-100">
+            {keys.map((key) => (
+              <div key={key}>
+                {console.log(`${name}`)}
+                {renderInput(`${name}.${index}.${key}`, key)}
+              </div>
+            ))}
+            <button
+              type="button"
+              onClick={() => fields.remove(index)}
+              className="btn btn-error btn-sm mt-2"
+            >
+              <FaTrash className="w-5 h-5 text-base-content" /> Remove {label}
+            </button>
+          </div>
+        ))}
+        <button
+          type="button"
+          onClick={() =>
+            fields.append(
+              keys.reduce((acc, key) => ({ ...acc, [key]: "" }), {})
+            )
+          }
+          className="btn btn-secondary btn-sm mt-2 flex items-center"
+        >
+          <FaPlusCircle className="w-5 h-5 mr-1 text-base-content" /> Add{" "}
+          {label}
+        </button>
+      </div>
+    );
+  };
+
   function showForm(){
     return (
         <div>
             <h1 className="text-4xl font-bold mb-6 flex items-center justify-center gap-2">
-            <FaBook className="text-accent" /> Resume</h1>
+            <FaBook className="text-accent" /> Resume Form</h1>
             <div>
-                <p className='py-4'>Personal Information</p>
-                <div className='grid grid-cols-12 gap-5'>
-                    <div className="col-span-12 lg:col-span-6">
-                      <label htmlFor='name'>Full Name</label>
-                        <input onChange={handleChange}
-                          type="text"
-                          name="name"
-                          id="name"
-                          placeholder="Type here"
-                          value={data.personalInformation.fullName}
-                          className="input input-bordered w-full" /> 
-                    </div>
-                    <div className="col-span-12 lg:col-span-6">
-                      <label htmlFor='email'>Email</label>
-                        <input onChange={handleChange}
-                          type="text" 
-                          name="email"
-                          id="email"
-                          placeholder="Type here" 
-                          value={data.personalInformation.email}
-                          className="input input-bordered w-full" />
-                    </div>
-                    <div className="col-span-12 lg:col-span-6">
-                      <label htmlFor='phoneNumber'>Phone Number</label>
-                      <input onChange={handleChange}
-                        type="text" 
-                        name="phoneNumber"
-                        id="phoneNumber"
-                        placeholder="Type here" 
-                        value={data.personalInformation.phoneNumber}
-                        className="input input-bordered w-full" 
-                      />
-                    </div>
-                    <div className="col-span-12 lg:col-span-6">
-                      <label htmlFor='location'>Location</label>
-                      <input onChange={handleChange}
-                        type="text" 
-                        name="location"
-                        id="location"
-                        placeholder="Type here" 
-                        value={data.personalInformation.location}
-                        className="input input-bordered w-full" 
-                      />
-                    </div>
-                    <div className="col-span-12 lg:col-span-6">
-                      <label htmlFor='linkedIn'>LinkedIn</label>
-                      <input onChange={handleChange}
-                        type="text" 
-                        name="linkedIn"
-                        id="linkedIn"
-                        placeholder="Type here" 
-                        value={data.personalInformation.linkedIn}
-                        className="input input-bordered w-full" 
-                      />
-                    </div>
-                    <div className="col-span-12 lg:col-span-6">
-                      <label htmlFor='gitHub'>GitHub</label>
-                      <input onChange={handleChange}
-                        type="text" 
-                        name="gitHub"
-                        id="gitHub"
-                        placeholder="Type here" 
-                        value={data.personalInformation.gitHub}
-                        className="input input-bordered w-full" 
-                      />
-                    </div>
-                    <div className="col-span-12 lg:col-span-12">
-                      <label htmlFor='portfolio'>Portfolio</label>
-                      <input onChange={handleChange}
-                        type="text" 
-                        name="portfolio"
-                        id="portfolio"
-                        placeholder="Type here" 
-                        value={data.personalInformation.portfolio}
-                        className="input input-bordered w-full" 
-                      />
-                    </div>
+               <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="p-6 space-y-6 bg-base-200 rounded-lg text-base-content"
+              >
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                  {renderInput("personalInformation.fullName", "Full Name")}
+                  {renderInput("personalInformation.email", "Email", "email")}
+                  {renderInput(
+                    "personalInformation.phoneNumber",
+                    "Phone Number",
+                    "tel"
+                  )}
+                  {renderInput("personalInformation.location", "Location")}
+                  {renderInput("personalInformation.linkedin", "LinkedIn", "url")}
+                  {renderInput("personalInformation.gitHub", "GitHub", "url")}
+                  {renderInput("personalInformation.portfolio", "Portfolio", "url")}
                 </div>
+
+                <h3 className="text-xl font-semibold">Summary</h3>
+                <textarea
+                  {...register("summary")}
+                  className="textarea textarea-bordered w-full bg-base-100 text-base-content"
+                  rows={4}
+                ></textarea>
+
+                {renderFieldArray(skillsFields, "Skills", "skills", [
+                  "title",
+                  "level",
+                ])}
+                {renderFieldArray(experienceFields, "Experience", "experience", [
+                  "jobTitle",
+                  "company",
+                  "location",
+                  "duration",
+                  "responsibility",
+                ])}
+                {renderFieldArray(educationFields, "Education", "education", [
+                  "degree",
+                  "university",
+                  "location",
+                  "graduationYear",
+                ])}
+                {renderFieldArray(
+                  certificationsFields,
+                  "Certifications",
+                  "certifications",
+                  ["title", "issuingOrganization", "year"]
+                )}
+                {renderFieldArray(projectsFields, "Projects", "projects", [
+                  "title",
+                  "description",
+                  "technologiesUsed",
+                  "githubLink",
+                ])}
+
+                <div className="flex gap-3 mt-16  p-4 rounded-xl ">
+                  <div className="flex-1">
+                    {renderFieldArray(languagesFields, "Languages", "languages", [
+                      "name",
+                    ])}
+                  </div>
+                  <div className="flex-1">
+                    {renderFieldArray(interestsFields, "Interests", "interests", [
+                      "name",
+                    ])}
+                  </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary w-full">
+                  Submit
+                </button>
+              </form>
+                
             </div>
 
         </div>
@@ -175,10 +240,10 @@ const DescriptionInputPage = () => {
   }
   return (
     <div className="mt-5 p-10 flex gap-3 flex-col items-center justify-center font-sans">
-        {showForm()}
+        {data && showForm()}
         {showInputFeild()}
     </div>
   );
 };
 
-export default DescriptionInputPage;
+export default GenerateResume;
